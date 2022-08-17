140170c2575ec0412202a643294e4908cd7599be
Mukul Mukhopadhyay <99615098+MukuJJay@users.noreply.github.com>
2022-08-17 20:30:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// ==============scroll effect===============
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home");
+
+const sectionOneOptions = {
+    rootMargin: "-80% 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries,
+    sectionOneObserver
+) {
+entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+        header.classList.add("header--scrolled");
+    } else {
+        header.classList.remove("header--scrolled");
+    }
+});
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
